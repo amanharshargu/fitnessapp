d54debe4f69c178f237d32ae9a2ae0aebdd1873b
amanharshargu <aman.choubey@argusoft.in>
2024-10-04 16:55:30 +0530

    fixed auth

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -21,8 +21,8 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response && error.response.status === 401) {
-      localStorage.removeItem('token');
-      window.location.href = '/login';
+      localStorage.removeItem("token");
+      window.location.href = "/login";
     }
     return Promise.reject(error);
   }
